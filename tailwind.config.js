3925a9d7bb90bb45fc639924c571e008cd72a66d
jkazuo55 <jkazuo555@gmail.com>
2022-02-11 16:41:39 -0400

    tailwind.config.js file was modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ['./src/**/*.{html,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      white: '#FFFFFF',
+      gray: {
+        500: '#0e101c',
+        200:"#666666"
+      },
+
+      pink: '#ec5990',
+      purple: '#081229',
+      pinkLight: '#bf1650'
+    },
+    extend: {
+      maxWidth: {
+        125: '31.25rem',
+      },
+    },
   },
   plugins: [],
 };
